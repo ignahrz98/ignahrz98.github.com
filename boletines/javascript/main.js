cdb4d9e0f9bc6efbc697a8397b7e464b65cf24f1
ignahrz98 <118219483+ignahrz98@users.noreply.github.com>
2022-11-16 16:08:30 -0400

    Completado módulo de Javascript del curso

---
+++ boletines/javascript/main.js
@@ -0,0 +1,55 @@
+
+
+// DOM
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+	console.log(link);
+});
+*/
+
+/*
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+	td.addEventListener('click', function(){
+		console.log(this);
+	});
+});
+*/
+
+//Obtener los elementos de la clase .close
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+links.forEach(function(link){
+	//Añadir un evento click a cada uno de ellos
+	link.addEventListener('click', function(ev){
+		ev.preventDefault();
+
+		//Quitarles las clases de animación que ya tiene.
+		let content  = document.querySelector(".content");
+
+		//Añadirle clases para animar su salida.
+		content.classList.remove("animate__fadeInDownBig");
+		content.classList.remove("animate__animated");
+
+		content.classList.add("animate__fadeOutUpBig");
+		content.classList.add("animate__animated");
+
+		setTimeout(function(){
+			location.href = "/";
+		}, 600);
+		
+	});
+});
+
+
+/*
+let iconos = document.querySelectorAll("i");
+iconos.forEach(function(icono){
+	icono.classList.remove("fa-star");
+	icono.classList.add("fa-star");
+});
+*/
