f5fddf4ce2f340ac94c2d1be8d003d9c64e665ce
ignahrz98 <118219483+ignahrz98@users.noreply.github.com>
2022-11-16 16:21:53 -0400

    Reparando la redirección

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -39,7 +39,7 @@ links.forEach(function(link){
 		content.classList.add("animate__animated");
 
 		setTimeout(function(){
-			location.href = "/";
+			location.href = "/boletines";
 		}, 600);
 		
 	});
